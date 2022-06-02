d84acfe4470a562cffdc7e072de190e1f0e955d1
Sachin Panayil <sachinpanayil01@gmail.com>
2022-06-02 14:49:17 -0400

    added dimness effect and multiple sounds

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound(title: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+        
+    }
+    
+    
+    func playSound(title: String) {
+            let url = Bundle.main.url(forResource: title, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+        }
 
     
 
